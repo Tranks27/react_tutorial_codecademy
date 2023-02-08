0f281639f2e7375b484f611ef1ee349c0eb40edc
TK_Surface <tranksntb@gmail.com>
2023-02-08 23:28:52 +1100

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+import logo from '../../logo.svg';
+import './App.css';
+
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
+
+const business ={
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+
+]
+function App() {
+  return (
+    <div className="App">
+      <h1>Naing's Yelp Web App</h1>
+      <SearchBar />
+      <BusinessList businesses={businesses}/>
+    </div>
+  );
+}
+
+export default App;
